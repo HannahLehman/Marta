f2bdf96f9b327c8a44df24d5f8a01d948c5f50ca
Hannah Lehman <hjlehman@gmail.com>
2014-12-16 19:00:28 -0500

    App using Marta API to show bus arrival times.

---
+++ app/helpers/application_helper.rb
@@ -0,0 +1,23 @@
+module ApplicationHelper
+
+	def hash_and_msg_generator(results, station)
+		@message = ""
+		@buses = {}
+		
+		results.each do |result|
+			if result["TIMEPOINT"].include? 'station'
+				@buses[result["ROUTE"]]=result["VEHICLE"]
+			end
+		end
+
+		if @buses.count == 0
+			@message = "Sorry, no buses are scheduled right now."
+		elsif @buses.count == 1
+			@message = "A bus is on it's way!"
+		elsif @buses.count > 1
+			@message = "These buses will be coming soon:"
+		else
+			@message = "Oops...I think there was a slight error."
+		end
+	end
+end
